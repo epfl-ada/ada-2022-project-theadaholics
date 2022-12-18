1402270b1a9974bcd00f28cf92b064c0d30f1ac9
nikolaystanishev <stanishev.nikolay@gmail.com>
2022-12-18 16:41:29 +0200

    Get pageviews only for pages which contains film or movie

--- utils/wikipedia_pageview_statistics.py
+++ utils/wikipedia_pageview_statistics.py
@@ -2,6 +2,8 @@ import pandas as pd
 import requests
 import urllib.parse
 
+import wikipedia
+
 from multiprocessing import Pool
 import tqdm
 
@@ -9,17 +11,19 @@ import tqdm
 def get_pageview_statistics(movie_name):
     result = []
     try:
-        query = "https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/en.wikipedia/all-access/user/" + \
-            f"{urllib.parse.quote(movie_name.replace(' ', '_')).replace('/', '%2F')}/monthly/2015070100/2022120300"
+        content = wikipedia.page(movie_name).content.lower()
+        if 'movie' in content or 'film' in content:
+            query = "https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/en.wikipedia/all-access/user/" + \
+                f"{urllib.parse.quote(movie_name.replace(' ', '_')).replace('/', '%2F')}/monthly/2015070100/2022120300"
 
-        response = requests.get(query, headers={
-            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.1 Safari/605.1.15'
-        })
-        pageview_stats = response.json()
-        for month in pageview_stats['items']:
-            result.append({'movie_name': movie_name, 'date': month['timestamp'], 'pageviews': month['views']})
+            response = requests.get(query, headers={
+                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.1 Safari/605.1.15'
+            })
+            pageview_stats = response.json()
+            for month in pageview_stats['items']:
+                result.append({'movie_name': movie_name, 'date': month['timestamp'], 'pageviews': month['views']})
     except:
-        print(f'problem: {movie_name}')
+        pass
 
     return result
 
@@ -37,6 +41,7 @@ def extract_wikipedia_pageview_statistics():
 
     movie_wikipedia_pageviews = pd.DataFrame(results)
     movie_wikipedia_pageviews.to_csv('./handled_data/movies_wikipedia_pageviews_raw.csv', index=False)
+    print(movie_wikipedia_pageviews.shape)
 
 
 def aggregate_wikipedia_pageview_statistics():
